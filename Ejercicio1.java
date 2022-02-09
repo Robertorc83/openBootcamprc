f4e0a07f1231a9e6b3bcdab3b02a2c9f3fcb6d06
Robertorc83 <50055949+Robertorc83@users.noreply.github.com>
2022-02-08 20:41:11 -0500

    Add files via upload

---
+++ Ejercicio1.java
@@ -0,0 +1,20 @@
+package com.OpenBootcamp;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        int numero1 = 1;
+        long numeroLong = 3456;
+
+        double numeroDouble = 2458d;
+        boolean bool1 = 5 < 10;
+        String Name = "Roberto";
+
+        System.out.println(numero1);
+        System.out.println(numeroLong);
+        System.out.println(numeroDouble);
+        System.out.println(bool1);
+        System.out.println(Name);
+
+    }
+}
